e6e81b1e96029553bb1be4189eb44419045420c2
Antonio Charneco <39918249+Elcharne@users.noreply.github.com>
2018-06-13 20:21:16 +0200

    Add files via upload

---
+++ media/HTML/Les_energies/java.js
@@ -0,0 +1,8 @@
+document.getElementById("Info").onclick = function(){
+            if(document.getElementById("Info").className == "Info_inicial"){
+                document.getElementById("Info").className ="Info_final";
+            }else{
+                document.getElementById("Info").className ="Info_inicial";
+            }
+        }
+
